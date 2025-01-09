c0047822f914dfa1eecfb7b36a10e5ac29131d30
hugo flores garcia <hugofg@u.northwestern.edu>
2025-01-09 17:43:17 -0500

    going back 2 a setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name="soundmaterial",
+    version="0.0.1",
+    description="audio data management for vampnet.",
+    long_description_content_type="text/markdown",
+    author="Hugo Flores García",
+    author_email="huferflo@gmail.com",
+    url="https://github.com/hugofloresgarcia/soundmaterial",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "pandas", 
+        "argbind",
+        "gradio"
+    ],
+)
